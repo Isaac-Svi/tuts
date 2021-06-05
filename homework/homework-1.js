55b6d48191a630433ab6bb64eae3df8fe4005303
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-06-05 23:27:22 +0300

    Add homework folder and first homework file

---
+++ homework/homework-1.js
@@ -0,0 +1,51 @@
+{
+  /**
+   * This is the question scope.  Don't touch anything in here
+   */
+  question1 = `
+Using the 'map' function, return a new array of the user ids plus 5
+`
+
+  question2 = `
+Using the 'filter' function, get an array of all users that have 'blue' as one of their favorite_colors.
+(Hint: the 'includes' function might help here)
+`
+
+  question3 = `
+Using the 'forEach' function, add a new key/value pair of "admin: true" to all users who are older than 30.
+(Hint: the 'filter' function might (or might not) help here :p)
+`
+}
+
+const users = [
+  {
+    id: 1,
+    name: 'Bob',
+    age: 34,
+    favorite_colors: ['blue', 'black', 'red'],
+  },
+  {
+    id: 2,
+    name: 'John',
+    age: 12,
+    favorite_colors: ['red', 'black', 'yellow'],
+  },
+  {
+    id: 3,
+    name: 'Jill',
+    age: 23,
+    favorite_colors: ['blue', 'green', 'orange'],
+  },
+  {
+    id: 4,
+    name: 'Kate',
+    age: 45,
+    favorite_colors: ['pink', 'white', 'red'],
+  },
+]
+
+// ---------- ANSWER 1 ----------------
+
+// ---------- ANSWER 2 ----------------
+
+// ---------- ANSWER 3 ----------------
